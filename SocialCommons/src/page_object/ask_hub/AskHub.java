7f15abe37914b70ec072038cff4132031f8926b5
Jpnrocks <jpnrocks@gmail.com>
2017-07-22 16:31:26 +0530

    Update AskHub.java

--- SocialCommons/src/page_object/ask_hub/AskHub.java
+++ SocialCommons/src/page_object/ask_hub/AskHub.java
@@ -1,27 +1,20 @@
-package ask_hub;
+package page_object;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import page_object.page_object_base;
 
 public class AskHub {
 
-
-	public static WebElement identity_iq(WebDriver driver){
-		 
-	    WebElement element = driver.findElement(By.linkText("New desk issues"));
-	 
-	    return element;
-	 
+	public static page_object_base identity_iq(WebDriver driver){
+		page_object_base page_base = new page_object_base();
+		page_base.element = driver.findElement(By.linkText("New desk issues"));
+	    return page_base;	 
 	    }
 	
-	public static WebElement video_title(WebDriver driver){
-		 
-	    WebElement element = driver.findElement(By.xpath(".//*[@id='jive-body-main']/div[2]/div/div[2]/header"));
-		
-	 
-	    return element;
-	 
+	public static page_object_base video_title(WebDriver driver){
+		page_object_base page_base = new page_object_base();
+		page_base.element = driver.findElement(By.xpath(".//*[@id='jive-body-main']/div[2]/div/div[2]/header"));
+	    return page_base;
 	    }
-
 }
