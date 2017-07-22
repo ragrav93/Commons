0bc0954694c7e5114d1fb8f301f45204d90c79f6
ragrav93 <raghul93diligent@ymail.com>
2017-07-22 11:26:22 +0530

    Create CommonsDashboard.java

---
+++ SocialCommons/src/page_object/CommonsDashboard.java
@@ -0,0 +1,47 @@
+package page_object;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class CommonsDashBoard {
+	private static WebElement element = null;
+	
+	public static WebElement top_destination(WebDriver driver){
+		 
+	    element = driver.findElement(By.linkText("Top Destinations"));
+	 
+	    return element;
+	 
+	    }
+	public static WebElement top_destination_more(WebDriver driver){
+		 
+	    element = driver.findElement(By.linkText("More..."));
+	 
+	    return element;
+	 
+	    }
+	public static WebElement commons_home_page(WebDriver driver){
+		 
+	    element = driver.findElement(By.id("logo"));
+	 
+	    return element;
+	 
+	    }
+	public static WebElement ask_hub(WebDriver driver){
+		 
+	    element = driver.findElement(By.linkText("askHub"));
+	 
+	    return element;
+	 
+	    }
+	
+	public static WebElement workday_link(WebDriver driver){
+
+        element = driver.findElement(By.linkText("Workday (PTO, Performance, Feedback)"));
+
+        return element;
+
+        }
+
+}
