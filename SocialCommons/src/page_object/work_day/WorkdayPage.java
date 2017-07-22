8cf24d8c5765e2d99d702e4efecc09a3f3a61d46
ragrav93 <raghul93diligent@ymail.com>
2017-07-22 11:17:17 +0530

    Create WorkdayPage.java

---
+++ SocialCommons/src/page_object/work_day/WorkdayPage.java
@@ -0,0 +1,15 @@
+package work_day_page_objects;
+import org.openqa.selenium.*;
+public class WorkdayPage {
+	
+private static WebElement element = null;
+	
+	public static WebElement workday_ul(WebDriver driver){
+		 
+        element = driver.findElement(By.className("WMPG"));
+
+        return element;
+
+        }
+
+}
