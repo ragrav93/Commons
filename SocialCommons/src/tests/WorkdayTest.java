a817237b2c706fd351f7c861de4990078b54653d
ragrav93 <raghul93diligent@ymail.com>
2017-07-23 21:35:57 +0530

    Update WorkdayTest.java

--- SocialCommons/src/tests/WorkdayTest.java
+++ SocialCommons/src/tests/WorkdayTest.java
@@ -1,4 +1,4 @@
-package work_day_tests;
+package tests;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -7,15 +7,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-import work_day_page_objects.CommonsDashboard;
-import work_day_page_objects.TimeoffPage;
-import work_day_page_objects.WorkdayPage;
+import page_object.CommonsDashboard;
+import page_object.TimeoffPage;
+import page_object.WorkdayPage;
+import page_object.TimeoffRequestPage;
 
 public class WorkdayTest {
 	
 	public static void navigate_to_workday(WebDriver driver) throws InterruptedException{
-	CommonsDashboard.top_destination(driver).click();
-        CommonsDashboard.workday_link(driver).click();
+	CommonsDashboard.top_destination(driver).click(5);
+        CommonsDashboard.workday_link(driver).click(5);
         ArrayList tabs = new ArrayList (driver.getWindowHandles());
         driver.switchTo().window((String) tabs.get(1));
 		Thread.sleep(20000);
@@ -28,6 +29,17 @@ public class WorkdayTest {
 		  Thread.sleep(10000);
 		  TimeoffPage.timeoff_button(driver).click();
 		  Thread.sleep(10000);
+	}
+	
+	public static String get_number_of_leaves(WebDriver driver, String leave_type) throws InterruptedException{
+		if(leave_type.equals("Casual Leave")){
+			return TimeoffRequestPage.casual_leave(driver).getText(5);
+		}else if (leave_type.equals("Privileged Leave")){
+			return TimeoffRequestPage.privileged_leave(driver).getText(5);
+		}else if (leave_type.equals("Sick Leave")){
+			return TimeoffRequestPage.sick_leave(driver).getText(5);
+		}
+		return "Not found";
 	}
 
 }
