d59e40e3de1ba7fea44345252a21a931df662249
ragrav93 <raghul93diligent@ymail.com>
2017-07-23 21:29:43 +0530

    Update AskHub.java

--- SocialCommons/src/page_object/ask_hub/AskHub.java
+++ SocialCommons/src/page_object/ask_hub/AskHub.java
@@ -2,18 +2,18 @@ package page_object;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import page_object.page_object_base;
+import page_object.PageObjectBase;
 
 public class AskHub {
 
-	public static page_object_base identity_iq(WebDriver driver){
-		page_object_base page_base = new page_object_base();
+	public static PageObjectBase identity_iq(WebDriver driver){
+		PageObjectBase page_base = new PageObjectBase();
 		page_base.element = driver.findElement(By.linkText("New desk issues"));
 	    return page_base;	 
 	    }
 	
-	public static page_object_base video_title(WebDriver driver){
-		page_object_base page_base = new page_object_base();
+	public static PageObjectBase video_title(WebDriver driver){
+		PageObjectBase page_base = new PageObjectBase();
 		page_base.element = driver.findElement(By.xpath(".//*[@id='jive-body-main']/div[2]/div/div[2]/header"));
 	    return page_base;
 	    }
